9cc4e8a68b1f64f61e9e200392b8e295c588996b
Zaed Abdullah <z4ed.thalib123@gmail.com>
2022-02-24 12:59:05 +0700

    Fix some layouting

--- lib/main.dart
+++ lib/main.dart
@@ -40,118 +40,133 @@ class _FormKalkulasi extends State<FormKalkulasi> {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(title: const Text("Kalkulator")),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Text("Hasil Perhitungan : $hasil\n"),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Input Pertama',
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Hasil Perhitungan : $hasil\n",
+                style: const TextStyle(fontSize: 20.0, fontWeight: FontWeight.w500)
               ),
-              controller: myController1,
-            ),
-            TextField(
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Input Kedua',
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Input Pertama',
+                  filled: true,
+                  fillColor: Color.fromARGB(255, 242, 242, 242),
+                ),
+                controller: myController1,
               ),
-              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-              keyboardType: TextInputType.number,
-              controller: myController2,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      int input1 = int.parse(myController1.text);
-                      int input2 = int.parse(myController2.text);
-                      hasil = (input1*input2) as double;
-                    });
-                  },
-                  child: const Text('X'),
-                  style: ElevatedButton.styleFrom(
-                    onPrimary: Colors.black87,
-                    primary: Colors.blue[300],
-                    minimumSize: const Size(88, 36),
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(2)),
+              const Text(''),
+              TextField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Input Kedua',
+                  filled: true,
+                  fillColor: Color.fromARGB(255, 242, 242, 242),
+                ),
+                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                keyboardType: TextInputType.number,
+                controller: myController2,
+              ),
+              const Text(''),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        int input1 = int.parse(myController1.text);
+                        int input2 = int.parse(myController2.text);
+                        hasil = (input1*input2) as double;
+                      });
+                    },
+                    child: const Text('X'),
+                    style: ElevatedButton.styleFrom(
+                      onPrimary: Colors.black87,
+                      primary: Color.fromARGB(255, 0, 140, 255),
+                      minimumSize: const Size(88, 36),
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(2)),
+                      ),
                     ),
                   ),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      int input1 = int.parse(myController1.text);
-                      int input2 = int.parse(myController2.text);
-                      hasil = input1/input2;
-                    });
-                  },
-                  child: const Text('/'),
-                  style: ElevatedButton.styleFrom(
-                    onPrimary: Color.fromARGB(221, 0, 0, 0),
-                    primary: Colors.blue[300],
-                    minimumSize: const Size(88, 36),
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(2)),
+                  const Text('        '),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        int input1 = int.parse(myController1.text);
+                        int input2 = int.parse(myController2.text);
+                        hasil = input1/input2;
+                      });
+                    },
+                    child: const Text('/'),
+                    style: ElevatedButton.styleFrom(
+                      onPrimary: Colors.black87,
+                      primary: const Color.fromARGB(255, 0, 140, 255),
+                      minimumSize: const Size(88, 36),
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(2)),
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      int input1 = int.parse(myController1.text);
-                      int input2 = int.parse(myController2.text);
-                      hasil = (input1+input2) as double;
-                    });
-                  },
-                  child: const Text('+'),
-                  style: ElevatedButton.styleFrom(
-                    onPrimary: Colors.black87,
-                    primary: Colors.blue[300],
-                    minimumSize: const Size(88, 36),
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(2)),
+                ],
+              ),
+              const Text(''),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        int input1 = int.parse(myController1.text);
+                        int input2 = int.parse(myController2.text);
+                        hasil = (input1+input2) as double;
+                      });
+                    },
+                    child: const Text('+'),
+                    style: ElevatedButton.styleFrom(
+                      onPrimary: Colors.black87,
+                      primary: const Color.fromARGB(255, 0, 140, 255),
+                      minimumSize: const Size(88, 36),
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(2)),
+                      ),
                     ),
                   ),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      int input1 = int.parse(myController1.text);
-                      int input2 = int.parse(myController2.text);
-                      hasil = (input1-input2) as double;
-                    });
-                  },
-                  child: const Text('-'),
-                  style: ElevatedButton.styleFrom(
-                    onPrimary: Colors.black87,
-                    primary: Colors.blue[300],
-                    minimumSize: const Size(88, 36),
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(2)),
+                  const Text('        '),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        int input1 = int.parse(myController1.text);
+                        int input2 = int.parse(myController2.text);
+                        hasil = (input1-input2) as double;
+                      });
+                    },
+                    child: const Text('-'),
+                    style: ElevatedButton.styleFrom(
+                      onPrimary: Colors.black87,
+                      primary: const Color.fromARGB(255, 0, 140, 255),
+                      minimumSize: const Size(88, 36),
+                      padding: const EdgeInsets.symmetric(horizontal: 17),
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(2)),
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
-        ),
+                ],
+              ),
+            ],
+          ),
+        )
       ),
     );
   }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,102 +1,102 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
+// // This is a basic Flutter widget test.
+// //
+// // To perform an interaction with a widget in your test, use the WidgetTester
+// // utility that Flutter provides. For example, you can send tap and scroll
+// // gestures. You can also use WidgetTester to find child widgets in the widget
+// // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_test/flutter_test.dart';
 
-import 'package:zaed_calculator/main.dart';
+// import 'package:zaed_calculator/main.dart';
 
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+// void main() {
+//   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+//     // Build our app and trigger a frame.
+//     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+//     // Verify that our counter starts at 0.
+//     expect(find.text('0'), findsOneWidget);
+//     expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+//     // Tap the '+' icon and trigger a frame.
+//     await tester.tap(find.byIcon(Icons.add));
+//     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
-}
+//     // Verify that our counter has incremented.
+//     expect(find.text('0'), findsNothing);
+//     expect(find.text('1'), findsOneWidget);
+//   });
+// }
 
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+// void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Retrieve Text Input',
-      home: MyCustomForm(),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       title: 'Retrieve Text Input',
+//       home: MyCustomForm(),
+//     );
+//   }
+// }
 
-// Define a custom Form widget.
-class MyCustomForm extends StatefulWidget {
-  const MyCustomForm({Key? key}) : super(key: key);
+// // Define a custom Form widget.
+// class MyCustomForm extends StatefulWidget {
+//   const MyCustomForm({Key? key}) : super(key: key);
 
-  @override
-  _MyCustomFormState createState() => _MyCustomFormState();
-}
+//   @override
+//   _MyCustomFormState createState() => _MyCustomFormState();
+// }
 
-// Define a corresponding State class.
-// This class holds the data related to the Form.
-class _MyCustomFormState extends State<MyCustomForm> {
-  // Create a text controller and use it to retrieve the current value
-  // of the TextField.
-  final myController = TextEditingController();
+// // Define a corresponding State class.
+// // This class holds the data related to the Form.
+// class _MyCustomFormState extends State<MyCustomForm> {
+//   // Create a text controller and use it to retrieve the current value
+//   // of the TextField.
+//   final myController = TextEditingController();
 
-  @override
-  void dispose() {
-    // Clean up the controller when the widget is disposed.
-    myController.dispose();
-    super.dispose();
-  }
+//   @override
+//   void dispose() {
+//     // Clean up the controller when the widget is disposed.
+//     myController.dispose();
+//     super.dispose();
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Retrieve Text Input'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: TextField(
-          controller: myController,
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        // When the user presses the button, show an alert dialog containing
-        // the text that the user has entered into the text field.
-        onPressed: () {
-          showDialog(
-            context: context,
-            builder: (context) {
-              return AlertDialog(
-                // Retrieve the text the that user has entered by using the
-                // TextEditingController.
-                content: Text(myController.text),
-              );
-            },
-          );
-        },
-        tooltip: 'Show me the value!',
-        child: const Icon(Icons.text_fields),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Retrieve Text Input'),
+//       ),
+//       body: Padding(
+//         padding: const EdgeInsets.all(16.0),
+//         child: TextField(
+//           controller: myController,
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         // When the user presses the button, show an alert dialog containing
+//         // the text that the user has entered into the text field.
+//         onPressed: () {
+//           showDialog(
+//             context: context,
+//             builder: (context) {
+//               return AlertDialog(
+//                 // Retrieve the text the that user has entered by using the
+//                 // TextEditingController.
+//                 content: Text(myController.text),
+//               );
+//             },
+//           );
+//         },
+//         tooltip: 'Show me the value!',
+//         child: const Icon(Icons.text_fields),
+//       ),
+//     );
+//   }
+// }
